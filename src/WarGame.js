0c2b175478b1c40673880d6795df66c32b403aac
Patrick Lee <patricklee1138@gmail.com>
2021-09-03 19:36:49 -0700

    WIP: adding card game methods

--- src/WarGame.js
+++ src/WarGame.js
@@ -31,3 +31,48 @@ class WarGame extends Component {
     this.draw_card.bind(this);    
   }
 }
+
+// Get a card from the deck.
+GetOneCardFromDeck = () => {
+  let rnd = Math.round(Math.random() * 52);
+  if (this.state.CardsDeck[rnd] != 0) {
+    this.state.CardsDeck[rnd] = 0;
+    return rnd;
+  }
+
+  // This card is absent; we need to get the next one
+  for (let count = rnd + 1; count < 52; count++) {
+    if (this.state.CardsDeck[count] != 0) {
+      this.state.CardsDeck[count] = 0;
+      return count;
+    }
+  }
+
+  // If the card and all those after it are missing (set to 0)...
+  // ...then we try looking at all the cards before it.
+  for (let count = rnd - 1; count >= 0; count--) {
+    if (this.state.CardsDeck[count] != 0) {
+      this.state.CardsDeck[count] = 0;
+      return count;
+    }
+  }
+
+  // If we are here, there are no cards in the deck.
+  return -1;
+}
+
+// Remove one card from the deck.
+MoveDeck = (deck) => {
+  for (let count = 0; count < 51; count++) {
+    deck[count] = deck[count + 1];
+  }
+  deck[51] = null;
+}
+
+GetCardCount = (deck) => {
+  let count;
+  for (count = 0; count < 52; count++) {
+    if (deck[count] == null) { break };
+  }
+  return count == 52 ? 0 : count++;
+}
